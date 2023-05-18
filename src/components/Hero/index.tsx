db641c69d59723094e9def1b9b0167f7ded23807
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-05-18 19:46:19 -0300

    ajuste no estilo da div container

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,13 @@
+import { Container } from '../../styles'
 import { Formulario, Titulo } from './styles'
 
 const Hero = () => (
   <Formulario>
-    <div className="container">
+    <Container>
       <Titulo>
         As melhores vagas para tecnologia, design e artes visuais.
       </Titulo>
-    </div>
+    </Container>
   </Formulario>
 )
 
