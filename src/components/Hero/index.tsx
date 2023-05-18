26f51e1d8977f38270b004fca4e1ad19550eb54f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-05-18 18:15:10 -0300

    hero alterado de CSS Module para CSS in JS

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { Formulario, Titulo } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <Formulario>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <Titulo>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      </Titulo>
     </div>
-  </form>
+  </Formulario>
 )
 
 export default Hero
