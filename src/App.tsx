2b89432372249f9a16c774b64e7d6892f4f60ff3
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-05-18 18:52:58 -0300

    alterado o global.css para um tema do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+import tema from './themes/theme'
 
-import './global.css'
+// import './global.css'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <div className="container">
-        <ListaVagas />
-      </div>
+      <ThemeProvider theme={tema}>
+        <EstiloGlobal />
+        <Header />
+        <Hero />
+        <Container>
+          <ListaVagas />
+        </Container>
+      </ThemeProvider>
     </>
   )
 }
